1c6d92e9626e6e4b493e581aa343de355e7e2607
Nicholette <nekonekonik@gmail.com>
2016-01-30 09:46:49 -0800

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dwarvesOfArcadiaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dwarvesOfArcadiaApp
+ */
+angular.module('dwarvesOfArcadiaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
